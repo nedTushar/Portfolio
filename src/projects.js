6105930d34084c07174d2c9800df51813a9849bc
tushardarlami <darlamitushar1234@gmail.com>
2023-06-07 17:14:54 +0530

    css change

--- src/projects.js
+++ src/projects.js
@@ -13,7 +13,7 @@ const projects = [
     imgURL: require("./assets/images/studyGpt.png"),
     title: "StudyGpt",
     description:
-      "A platform for school students who can ask their any doubts related to study that even their teacher can't solve it.",
+      " A platform for school students who can ask their any doubts related to study that even their teacher can't solve it.",
     github: "https://github.com/nedTushar/studyGpt",
     live: "https://studygpt.netlify.app/",
   },
